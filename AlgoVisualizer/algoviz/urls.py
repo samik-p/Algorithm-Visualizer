131ba440ffe4c23a41e01df45e81b0e1d7681048
Samik Pradhan <samikpradhan@gmail.com>
2024-09-12 15:11:10 -0700

    testing views

--- AlgoVisualizer/algoviz/urls.py
+++ AlgoVisualizer/algoviz/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:algo_id>/", views.array_viz, name="array_viz"),
 ]
--- AlgoVisualizer/algoviz/views.py
+++ AlgoVisualizer/algoviz/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("This is the algoviz index")
+
+
+def array_viz(request, algo_id):
+    response = f"You're looking at the array visualization for algo id {algo_id}"
+    return HttpResponse(response)
